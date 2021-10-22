01a5b625db759b9e71265008a0c73614d82ea432
Bobby Nannier <bobby.nannier@economical.com>
2021-10-21 20:55:25 -0400

    g

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import classNames from 'classnames';
 import PropTypes from "prop-types";
-import button from './button.module.scss';
+import button from './button.scss';
 
 const Button = (props) => {
     const onClick = () =>{
